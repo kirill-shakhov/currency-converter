a9f27bf57721d284d1044276bf300908b74cd1bd
кирилл <shakhov-work@yandex.ru>
2024-03-02 12:39:26 +0300

    master feat( app ): added components, 'router-view'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-container>
+    <app-header/>
+
+    <router-view/>
+  </app-container>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import AppHeader from "./shared/components/AppHeader/AppHeader.vue";
+import AppContainer from "./shared/components/AppContainer/AppContainer.vue";
+
+</script>
+
+<style scoped>
+
+</style>
