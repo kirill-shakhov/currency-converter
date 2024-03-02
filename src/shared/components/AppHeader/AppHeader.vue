a9378e4b3b630d7ebd48f4e1441f71fabfc6cf3e
кирилл <shakhov-work@yandex.ru>
2024-03-02 12:31:51 +0300

    master feat( AppHeader ): added component 'app-header'

---
+++ src/shared/components/AppHeader/AppHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="app-header">
+    <div class="buttons-group">
+
+      <router-link :to="{name: 'currency-list'}">
+        <ui-button>
+          Валюты
+        </ui-button>
+      </router-link>
+
+      <router-link :to="{ name: 'currency-converter' }">
+        <ui-button
+            theme="secondary"
+        >
+          Конвертер
+        </ui-button>
+      </router-link>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { UiButton } from "../UiButton";
+</script>
+
+<style lang="scss">
+.app-header {
+  padding-top: 20px;
+}
+  .buttons-group {
+    display: flex;
+    gap: 12px;
+  }
+</style>
