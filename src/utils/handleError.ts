33b642fd9d1a10204d2bae0f1b1e8e8239159df3
кирилл <shakhov-work@yandex.ru>
2024-03-02 16:58:20 +0300

    master feat( app ): added type for param 'e'

--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // доработать
-export function handleError(e) {
+export function handleError(e: unknown) {
     if (axios.isAxiosError(e)) {
         if (e?.response?.data) {
             const errors = e?.response?.data;
