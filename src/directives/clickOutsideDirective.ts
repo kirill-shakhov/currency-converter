772884750a4db7e34a5386951122f4c88d3b1ae0
кирилл <shakhov-work@yandex.ru>
2024-03-02 13:10:45 +0300

    master feat( clickOutsideDirective ): added directive

---
+++ src/directives/clickOutsideDirective.ts
@@ -0,0 +1,18 @@
+import { ObjectDirective } from 'vue';
+
+const clickOutside: ObjectDirective = {
+    beforeMount(el, binding) {
+        el.clickOutsideEvent = function(event: MouseEvent) {
+            if (!(el === event.target || el.contains(event.target as Node))) {
+                binding.value(event); // вызываем метод
+            }
+        };
+        document.addEventListener('click', el.clickOutsideEvent, true);
+    },
+    unmounted(el) {
+        document.removeEventListener('click', el.clickOutsideEvent, true);
+    },
+
+};
+
+export default clickOutside;
