0a890dec8f229b098db155067dfb1808c7241246
кирилл <shakhov-work@yandex.ru>
2024-03-06 13:56:05 +0300

    master feat( App-header ): added class 'active' for current route

--- src/shared/components/AppHeader/AppHeader.vue
+++ src/shared/components/AppHeader/AppHeader.vue
@@ -3,13 +3,16 @@
     <div class="buttons-group">
 
       <router-link :to="{name: 'currency-list'}">
-        <ui-button>
+        <ui-button
+            :class="{ 'active': isActiveRoute('currency-list') }"
+        >
           Валюты
         </ui-button>
       </router-link>
 
       <router-link :to="{ name: 'currency-converter' }">
         <ui-button
+            :class="{ 'active': isActiveRoute('currency-converter') }"
             theme="secondary"
         >
           Конвертер
@@ -21,14 +24,23 @@
 
 <script setup lang="ts">
 import { UiButton } from "../UiButton";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const isActiveRoute = (routeName: string): boolean => {
+  return route.name === routeName;
+};
+
 </script>
 
 <style lang="scss">
 .app-header {
   padding-top: 20px;
 }
-  .buttons-group {
-    display: flex;
-    gap: 12px;
-  }
+
+.buttons-group {
+  display: flex;
+  gap: 12px;
+}
 </style>
